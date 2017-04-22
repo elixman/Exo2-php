d80bde62e25389d201919acd4a8443405256790f
elixman <sylvain.barriere@icloud.com>
2017-04-22 11:08:35 +0200

    Title updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title></title>
+    <title>Exo2-php</title>
   </head>
   <body>
     <p>##Exercice 1</p>
